2594fda7c7948ba22ea098ae8fd2e799cd826285
Anna Springfield <anna.e.springfield@gmail.com>
2017-03-13 13:37:00 -0500

    initial commit

---
+++ client.js
@@ -0,0 +1,21 @@
+$(document).ready(onReady);
+
+function onReady() {
+  $(".generate").data("clickCount", 0);
+  $(".generate").on("click", function () {
+    $(this).parent().append("<div></div>");
+    var clickCount = $(this).data("clickCount");
+    clickCount++;
+    $(this).data("clickCount", clickCount);
+    var $newDiv = $(this).siblings().last();
+    $newDiv.addClass("red");
+    $newDiv.append("<p>" + clickCount + "</p>");
+    $newDiv.append("<button class='swap'>Swap</button><button class='delete'>Delete</button>");
+  });
+  $(".container").on("click", ".swap", function() {
+    $(this).parent().removeClass("red").addClass("yellow");
+  });
+  $(".container").on("click", ".delete", function() {
+    $(this).parent().remove();
+  });
+}
